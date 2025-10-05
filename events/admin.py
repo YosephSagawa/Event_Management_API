8babc9a3e6f70587c14e394509caf1ba7c70baa5
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-05 09:09:20 +0300

    Created a super user and added the models to the admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Event, Category, Favorite, Registration
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Event)
+admin.site.register(Category)
+admin.site.register(Favorite)
+admin.site.register(Registration)
