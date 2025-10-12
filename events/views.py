f7e1d3d13b1fc96aa1ef70d1372bc1b3cd719903
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-12 11:32:47 +0300

    Added the Event views and their respective permission classes and filtering querying logic

--- events/views.py
+++ events/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer, EventSerializer
 from django.contrib.auth import get_user_model
+from .permissions import IsOwnerOrReadOnly
+from .models import Event
+from django.utils import timezone
+from django.db.models import Q
 
 User = get_user_model()
 
@@ -23,3 +27,33 @@ class RegisterView(generics.CreateAPIView):
     
 class LoginView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class EventListCreateView(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated] # Must be logged in to list and create events
+
+    def get_queryset(self):
+        # Filter events that are in the future
+        queryset = Event.objects.filter(date__gt = timezone.now())
+        # Filter events based on query parameters
+        title = self.request.query_params.get('title')
+        location = self.request.query_params.get('location')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+        if start_date:
+            queryset = queryset.filter(date__gte=start_date)
+        if end_date:
+            queryset = queryset.filter(date__lte=end_date)
+
+        return queryset
+    
+class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly] #Authenticated and logged in + ownership for update/delete
