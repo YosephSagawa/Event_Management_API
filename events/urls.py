bf5ed3e49de40661fe76ccfb8d806986d55280da
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-12 11:40:37 +0300

    Added the EventListCreate View and EventDetailView to the urls

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, EventListCreateView, EventDetailView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("events/", EventListCreateView.as_View(), name='event-list-create'),
+    path("events/<int:pk>/", EventDetailView.as_view(), name='event-detail'),
 ]
