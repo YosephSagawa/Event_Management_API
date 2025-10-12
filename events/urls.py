58e075d534264fac75a1734c4b768fd8feac9135
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-10-12 11:55:37 +0300

    Added the user management views to the urls

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, EventListCreateView, EventDetailView
+from .views import RegisterView, LoginView, EventListCreateView, EventDetailView, UserListView,UserDetailView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
-    path("events/", EventListCreateView.as_View(), name='event-list-create'),
+    path("events/", EventListCreateView.as_view(), name='event-list-create'),
     path("events/<int:pk>/", EventDetailView.as_view(), name='event-detail'),
+    path("users/", UserListView.as_view(), name='user-list'),
+    path("users/<int:pk>/", UserDetailView.as_view(), name='user-detail'),
 ]
